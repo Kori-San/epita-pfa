c71fdcc9b3140a919233cb983ffa2db0d9265866
karimsalhi <karimsalhi.contact@gmail.com>
2024-03-27 13:50:39 +0100

    Dropdown logic implemented

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,18 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import faker from 'faker';
-import mergedData from '../export/dist/export-2024-03-25_13-03-12-714847546/merged_data.json';
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import mergedData from "../export/dist/export-2024-03-25_13-03-12-714847546/merged_data.json";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import React from "react";
 
 ChartJS.register(
   CategoryScale,
@@ -25,29 +33,29 @@ ChartJS.register(
 export const options = {
   responsive: true,
   interaction: {
-    mode: 'index' as const,
+    mode: "index" as const,
     intersect: false,
   },
   stacked: false,
   plugins: {
     title: {
       display: true,
-      text: 'Chart.js Line Chart - Multi Axis',
+      text: "Chart.js Line Chart - Multi Axis",
     },
   },
   scales: {
     y: {
-      type: 'linear' as const,
+      type: "linear" as const,
       display: true,
-      position: 'left' as const,
+      position: "left" as const,
     },
     y1: {
-      type: 'linear' as const,
+      type: "linear" as const,
       display: true,
-      position: 'right' as const,
+      position: "right" as const,
       grid: {
         drawOnChartArea: false,
-      }
+      },
     },
   },
 };
@@ -80,34 +88,102 @@ const acidityLabels = mergedData.map((x) => x.hawaii_acidity[0].hawaii_ph);
 
 const degreeLabels = mergedData.map((x) => x.global_sea_year_anomaly_farenheit);
 
-
 export const data = {
   labels,
   datasets: [
     {
-      label: 'Carbon Dioxidre (ppm)',
+      label: "Carbon Dioxidre (ppm)",
       data: carbonLabels,
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      yAxisID: 'y',
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      yAxisID: "y",
     },
     {
-      label: 'Ph',
+      label: "Ph",
       data: acidityLabels,
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      yAxisID: 'y1',
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+      yAxisID: "y1",
     },
     {
-      label: 'Farenheit',
+      label: "Farenheit",
       data: degreeLabels,
-      borderColor: 'rgb(10, 50, 150)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      yAxisID: 'y1',
+      borderColor: "rgb(10, 50, 150)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+      yAxisID: "y1",
     },
   ],
 };
 
 export default function App() {
-  return <Line options={options} data={data} />;
+  const [data1, setData1] = React.useState("");
+  const [data2, setData2] = React.useState("");
+
+  const [displayData, setDisplayData] = React.useState({ labels: [], datasets: [] });
+
+
+  const handleChange1 = (event: SelectChangeEvent) => {
+    setData1(event.target.value as string);
+    const newDataset = data.datasets.find(dataset => dataset.label === event.target.value);
+    if (newDataset) {
+      setDisplayData(prevData => ({
+        labels: data.labels,
+        datasets: [newDataset, prevData.datasets[1]] // Replace the first dataset
+      }));
+    }
+  };
+
+  const handleChange2 = (event: SelectChangeEvent) => {
+    setData2(event.target.value as string);
+    const newDataset = data.datasets.find(dataset => dataset.label === event.target.value);
+    if (newDataset) {
+      setDisplayData(prevData => ({
+        labels: data.labels,
+        datasets: [prevData.datasets[0], newDataset] // Replace the second dataset
+      }));
+    }
+  };
+
+  const selectData1 = [data.datasets[0].label, data.datasets[1].label, data.datasets[2].label];
+  const selectData2 = [data.datasets[0].label, data.datasets[1].label, data.datasets[2].label];
+  return (
+    <>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Graph Data 1</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={data1}
+            label="Data 1"
+            onChange={handleChange1}
+          >
+            {selectData1.map((value) => (
+              <MenuItem key={value} value={value}>
+                {value}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Graph Data 2</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={data2}
+            label="Data 2"
+            onChange={handleChange2}
+          >
+            {selectData2.map((value) => (
+              <MenuItem key={value} value={value}>
+                {value}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <Line options={options} data={displayData} />
+    </>
+  );
 }
